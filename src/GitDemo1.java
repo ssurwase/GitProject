f068d2505cee62746041e0e776671902975fbbee
ssurwase <swapnil.surwase@gmail.com>
2018-02-25 13:24:36 +0530

    dddd

--- src/GitDemo1.java
+++ src/GitDemo1.java
@@ -2,13 +2,34 @@ import org.testng.annotations.Test;
 
 public class GitDemo1 {
 	
-	
-	
-	
+		
 	@Test
 	public void login()
 	{
 		System.out.println("you are in login test case");
+	}
+	
+	@Test
+	public void login1()
+	{
+		System.out.println("you are in login1 test case");
+	}
+	
+	@Test
+	public void login2()
+	{
+		System.out.println("you are in login2 test case");
+	}
+	
+	@Test
+	public void login3()
+	{
+		System.out.println("you are in login3 test case");
+	}
+	@Test
+	public void login4()
+	{
+		System.out.println("you are in login4 test case");
 	}
 
 }
